4001b3edfb6a621b0baf9cef7c2d92b853472b4a
JT <bodybybuddha@imneverwrong.com>
2026-02-21 21:45:19 +0000

    Fix branch protection script for personal repos

--- .github/setup_branch_protection.sh
+++ .github/setup_branch_protection.sh
@@ -40,19 +40,15 @@ setup_branch_protection() {
   },
   "enforce_admins": true,
   "required_pull_request_reviews": {
-    "dismissal_restrictions": {},
     "dismiss_stale_reviews": true,
     "require_code_owner_reviews": true,
-    "required_approving_review_count": 1,
-    "require_last_push_approval": false
+    "required_approving_review_count": 1
   },
   "restrictions": null,
   "required_linear_history": true,
   "allow_force_pushes": false,
   "allow_deletions": false,
-  "required_conversation_resolution": true,
-  "lock_branch": false,
-  "allow_fork_syncing": false
+  "required_conversation_resolution": true
 }
 EOF
     else
@@ -66,19 +62,15 @@ EOF
   },
   "enforce_admins": false,
   "required_pull_request_reviews": {
-    "dismissal_restrictions": {},
     "dismiss_stale_reviews": true,
     "require_code_owner_reviews": false,
-    "required_approving_review_count": 0,
-    "require_last_push_approval": false
+    "required_approving_review_count": 0
   },
   "restrictions": null,
   "required_linear_history": false,
   "allow_force_pushes": false,
   "allow_deletions": false,
-  "required_conversation_resolution": true,
-  "lock_branch": false,
-  "allow_fork_syncing": false
+  "required_conversation_resolution": true
 }
 EOF
     fi
